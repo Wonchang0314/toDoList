27e640d997c8b5f4928a0f7de4fb6683dfdd9785
Wonchang0314 <andrew0314@naver.com>
2024-08-23 12:12:21 +0900

    FIX: ejs파일을 동적파일로 분류하도록 변경

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", function (req, res) {
   res.render("index");
 });
